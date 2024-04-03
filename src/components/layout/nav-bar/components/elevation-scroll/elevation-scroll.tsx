288746c8d4efaa9fba5d69b85ee21411694f3dc9
pauloWeslleY <weslleyspfc@13outlook.com>
2024-04-03 11:04:37 -0300

    refactor: components elevation-scroll

--- src/components/layout/nav-bar/components/elevation-scroll/elevation-scroll.tsx
+++ src/components/layout/nav-bar/components/elevation-scroll/elevation-scroll.tsx
@@ -3,14 +3,14 @@ import { useScrollTrigger } from '@mui/material'
 import { Props } from '../../nav-bar-props'
 
 export const ElevationScroll = (props: Props) => {
-  const { children, window } = props
+  const { children, windows } = props
   // Note that you normally won't need to set the window ref as useScrollTrigger
   // will default to window.
   // This is only being set here because the demo is in an iframe.
   const trigger = useScrollTrigger({
     disableHysteresis: true,
     threshold: 0,
-    target: window ? window() : undefined,
+    target: windows ? windows() : undefined,
   })
 
   return cloneElement(children, {
